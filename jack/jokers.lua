c319d20d7a6582726a6f4128f27b33293421c056
kxeine <nsymxd@gmail.com>
2025-06-10 17:55:23 +0100

    Add files via upload

---
+++ jack/jokers.lua
@@ -0,0 +1,56 @@
+SMODS.Atlas{
+    key="jkr",
+    path="jokers.png",
+    px=71,
+    py=95,
+
+
+
+
+
+}
+
+
+
+
+
+
+SMODS.Joker{
+    key="maid",
+    atlas="jkr",
+    pos={x=0,y=0},
+    rarity=1,
+    cost=3,
+    loc_txt = {
+        name = "Lucky Turn",
+        text = {
+            "{C:green}#1# in #2# chance{} to gain {C:attention}+1 hand{}",
+            "each time a hand is played",
+            "Current Bonus: {C:attention}+#3# hand{}"
+        }
+    },
+    config = { extra = { hands = 0, odds = 8 } }, -- Store hand count and odds (1 in 2)
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true, -- Compatible with Blueprint Joker
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.after and not context.blueprint then
+            -- Check for 1 in 2 chance using proper probability calculation
+            if pseudorandom("lucky_turn") < G.GAME.probabilities.normal / card.ability.extra.odds then
+                -- Increase hand count bonus
+                card.ability.extra.hands = card.ability.extra.hands + 1
+                G.GAME.round_resets.hands = G.GAME.round_resets.hands + 1
+                return {
+                    message = "+1",
+                    card = card,
+                    colour = G.C.BLUE
+                }
+            end
+        end
+    end,
+    loc_vars = function(self, info_queue, card)
+        -- Update description with "1 in 2" and current hand count
+        return { vars = { 1, card.ability.extra.odds, card.ability.extra.hands } }
+    end
+}
+
